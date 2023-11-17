35ef40a52e3927710e60c559b4b17b5d8dd63be2
XuanTam Hoang <xuantam.hoang23@gmail.com>
2023-11-17 08:52:53 +0100

    Changed typo to about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,7 @@ const About = () => {
             <div className="container flex-container flex-vertical-center">
                 <div className="about-description">
                     <p className="section-subtitle">01.</p>
-                    <h2 className="section-title">Un développeur passionné et hyper-connecté</h2>
+                    <h2 className="section-title">Développeur passionné et hyper-connecté</h2>
                     <p>Bonjour ! Je m'appelle Xuan-Tam Hoang. Diplômé d'un Master spécialisé dans la conception de sites Web et actuellement en formation intégralement à distance chez OpenClassrooms,
                     je me spécialise dans la réalisation de <strong>sites vitrines</strong> et <strong>e-commerce</strong>. Fort d'une expérience de deux ans en agence Web, je suis capable de concevoir des <strong>sites ou applications responsive</strong> tout en travaillant en équipe.</p>
                     <p>À l'issue de cette formation enrichissante, je souhaite me spécialiser dans le développement d'<strong>applications Web modernes sur React</strong>. Grâce à la veille technologique que j'effectue au quotidien, je reste tenu au courant des dernières technologies et standards du Web.</p>
