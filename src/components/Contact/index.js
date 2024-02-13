037db5bf3a8f8e253a723f72a07a307c3fb1509e
XuanTam Hoang <xuantam.hoang23@gmail.com>
2024-02-13 10:15:23 +0100

    Edited colors, added font weight variable

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,7 +4,7 @@ const Contact = () => {
             <p className="section-subtitle">04.</p>
             <h2 className="section-title">Et si on discutait de votre projet ?</h2>
             <p>Disponible dès maintenant, prenons un café et discutons de votre projet !</p>
-            <a href="mailto:xuantam.hoang23@gmail.com" className="btn">Contact</a>
+            <a href="mailto:xuantam.hoang23@gmail.com" className="btn btn-secondary">Contact</a>
         </section>
     );
 };
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -18,7 +18,7 @@ const Hero = () => {
                 <img src={hero} alt={"test"} />
             </div>
             <div className="hero-content">
-                <h1><span>Xuan-Tam</span> Hoang</h1>
+                <h1>Xuan-Tam <span>Hoang</span></h1>
                 <h2>Développeur Front-End</h2>
                 <div className="hero-cta flex-container flex-vertical-center" data-aos="fade-up" data-aos-duration="600">
                     <a className="btn btn-primary" href={ResumePDF} target="_blank" rel="noreferrer">Télécharger mon CV</a>
