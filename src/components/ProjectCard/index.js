25b1032dcfbe34ef483c228065ba3616a08d4789
XuanTam Hoang <xuantam.hoang23@gmail.com>
2024-02-14 10:27:13 +0100

    Removed Github icon link in Project component, edited hover animation

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,23 +1,21 @@
 import Tag from '../Tag';
 import './ProjectCard.scss';
-import github from './../../assets/images/github-mark-white.svg';
 
 const ProjectCard = ({title, summary, description, tags, image, url}) => {
 
     return (
         <>
         <div className="project-item" data-aos="fade-up" data-aos-duration="600">
-            <div className="project-image">
-                {image ? <img src={image} alt={title}/> : <img src="https://placehold.co/600x400" alt="Project cover"/>}
-            </div>
+            {url && 
+                <a href={url} target="_blank" rel="noreferrer" className="project-link">
+                    <div className="project-image">
+                        {image ? <img src={image} alt={title}/> : <img src="https://placehold.co/600x400" alt="Project cover"/>}
+                    </div>
+                </a>
+            }
             <div className='project-description'>
                 <div className='project-header'>
                     <h3 className="project-title">{title}</h3>
-                    {url && 
-                        <a href={url} target="_blank" rel="noreferrer">
-                            <img src={github} alt="Logo Github"/>
-                        </a>
-                    }
                 </div>
                 <ul className="project-tags">
                     {tags.map((tag,index) => 
